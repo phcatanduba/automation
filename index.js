8d6e9c7d2c010582235ca44f693760b2d90a9fca
phcatanduba@gmail.com <phcatanduba@gmail.com>
2023-03-09 05:46:06 -0300

    login add

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-
 const {Builder, By, Key} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const chromedriver = require('chromedriver');
+require('dotenv').config({path: __dirname + '/.env'});
 
-chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+const login = process.env.login
+const password = process.env.password
+const link = process.env.link
+
+console.log(login, password, link)
+
+let driver = undefined
 
 async function run() {
-    let driver = await new Builder().forBrowser('chrome').build();
-    await driver.get('https://google.com');
-    const searchBar = await driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
-    await searchBar.sendKeys("nodejs");
-    await searchBar.sendKeys(Key.ENTER);
+    driver = await new Builder().forBrowser('chrome').build();
+    await driver.get(link);
+    const loginBar = await getElement("/html/body/div[2]/div[3]/div/div[2]/form/div[1]/input");
+    await loginBar.sendKeys(login);
+    const passwordBar = await getElement("/html/body/div[2]/div[3]/div/div[2]/form/div[3]/input");
+    await passwordBar.sendKeys(password);
+    const enterButton = await getElement("/html/body/div[2]/div[3]/div/div[2]/form/div[5]/button");
+    await enterButton.click();
+    await driver.get("www.google.com");
+}
+
+async function getElement(xpath) {
+    return await driver.findElement(By.xpath(xpath))
 }
 
 run();
